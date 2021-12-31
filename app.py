eb6cf33761750d37f32657c4ae69df5e9e57ca73
ip69719 <ipeters@mail.com>
2021-12-31 02:01:11 +0000

    Update add_recipe functionality to allow image upload

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 from flask import (
     Flask, flash, render_template,
     redirect, request, session, url_for)
@@ -32,40 +33,67 @@ def get_recipes():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
-        # Check if Username already exists in the DB
+        # check if username already exists in the db
         existing_user = mongo.db.users.find_one(
             {"username": request.form.get("username").lower()})
 
         if existing_user:
-            flash("Username already exists")
+            flash("Username already exists! Please try again.")
             return redirect(url_for("register"))
-        
+
         register = {
             "username": request.form.get("username").lower(),
             "password": generate_password_hash(request.form.get("password"))
         }
         mongo.db.users.insert_one(register)
 
-        # Put the new user into 'session' cookie
+        # put the new user into 'session' cookie
         session["user"] = request.form.get("username").lower()
         flash("Registration complete!")
     return render_template("register.html")
 
 
-@app.route("/add_recipe", methods=["GET","POST"])
+@app.route("/add_recipe", methods=["GET", "POST"])
 def add_recipe():
     if request.method == "POST":
+        #UI needs to be updated to include all fields
+        # add image to database
+        recipe_image = None
+        result = None
+        if 'recipe_image' in request.files:
+            recipe_image = request.files['recipe_image']
+            if recipe_image.filename != '':
+                # generate filename using an uuid
+                recipe_image.filename = str(uuid.uuid4())
+                result = mongo.save_file(recipe_image.filename, recipe_image)
+
         recipe = {
             "recipe_name": request.form.get("recipe_name"),
             "category_name": request.form.get("category_name"),
-            "ingredients": request.form.get("ingredients"),
-            "method": request.form.get("method")
+            "ingredients": request.form.get("ingredients").split('\n'),
+            "method": request.form.get("method").split('\n'),
+            "image_filename": recipe_image.filename,
+            "image_id": result
         }
+
         mongo.db.recipes.insert_one(recipe)
-        flash("Recipe added!") # not working
+        flash("Recipe added!") # not working yet
         
     categories = mongo.db.categories.find()
     return render_template("add_recipe.html", categories=categories)
+
+
+@app.route("/edit_recipe/<recipe_id>", methods=["GET", "POST"])
+def edit_recipe(recipe_id):
+    recipe = mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+
+    categories = mongo.db.categories.find()
+    return render_template("edit_recipe.html", recipe=recipe, categories=categories)
+
+
+@app.route("/img_uploads/<filename>")
+def img_uploads(filename):
+    return mongo.send_file(filename)
 
 
 if __name__ == "__main__":
