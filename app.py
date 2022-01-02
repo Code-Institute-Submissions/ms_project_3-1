f53fa5ac1c390b71b21a80274a8928e2fab57d8f
ip69719 <ipeters@mail.com>
2022-01-02 11:18:09 +0000

    Change debug to False

--- app.py
+++ app.py
@@ -126,4 +126,4 @@ def img_uploads(filename):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
